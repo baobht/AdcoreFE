e23fecff6f4682f4b70aa32583764089147df2d2
baobht <binpn1603@gmail.com>
2024-11-04 10:23:52 +0700

    Update layout.tsx

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/global/footer";
 import Navbar from "@/components/global/navbar";
+import { Suspense } from "react";
 
 type LandingLayoutProps = {
   children: React.ReactNode;
@@ -9,7 +10,7 @@ const LandingLayout = ({ children }: LandingLayoutProps) => {
   return (
     <div className="relative min-h-dvh grid grid-rows-[auto_1fr_auto]">
       <Navbar />
-      {children}
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
       <Footer />
     </div>
   );
